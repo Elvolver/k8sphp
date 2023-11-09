7389d8a70a331822ee8ea51b65c7615362fa2865
elvolver <elvolver@mail.ru>
2023-11-09 16:52:27 +0300

    Version 1

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $ip_server = $_SERVER['SERVER_ADDR'];
 
 echo "<h1>Hello from Kubernetes</h1><br>";
+echo "<h1>Version 1</h1><br>";
 echo "Server IP Address is: $ip_server", "<br>";
 echo "<p>Made by <font color=blue>Dimon Volkov</p>"
 
